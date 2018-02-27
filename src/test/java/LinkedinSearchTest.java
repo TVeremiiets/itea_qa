c4b8480aa5006ae6dbd3d14b4aad4ed001950a06
tveremiiets <tveremiiets@gmail.com>
2018-02-27 20:43:07 +0200

    v_25

--- src/test/java/LinkedinSearchTest.java
+++ src/test/java/LinkedinSearchTest.java
@@ -1,17 +1,24 @@
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
 public class LinkedinSearchTest {
 
-    public class LinkedinLoginTest {
+
         WebDriver driver;
 
         @BeforeMethod
         public void beforeTest () {
             driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.get("https://www.linkedin.com/");
         }
         @AfterMethod
@@ -20,16 +27,54 @@ public class LinkedinSearchTest {
         }
 
         @Test
-        public void basicSearchTest(){
-            LinkedInLoginPage loginPage=new LinkedInLoginPage (driver);
+        public void basicSearchTest() throws InterruptedException {
+            LinkedInLoginPage loginPage = new LinkedInLoginPage (driver);
             loginPage.loginAs("TanyaQA07@gmail.com", "0635663551");
 
             // search
-            //input [@placeholder='Search']
-            //*[@type='search-icon']
-            //div[contains(@class, 'search-results-person')]
+            String searchTerm = "hr";
+
+            driver.findElement(By.xpath("//input[@placeholder='Поиск']")).sendKeys( searchTerm);
+            driver.findElement(By.xpath ("//*[@type='search-icon']")).click();
+            sleep(5000);
+            // driver.findElement(By.xpath ("//div[contains(@class, 'search-results-person')]"));
+
+            List<WebElement> results =driver.findElements(By.xpath("//li[contains(@class,'search-result__occluded-item')]"));
+            int currentResultNumber=results.size();
+            Assert.assertEquals(currentResultNumber, 10,"Number of results is wrong");
+            //sleep(5000);
+
+            // скролим  --- HomeTask пофиксить цикл
+           /* for (int i=1; i < results.size(); i++) {
+                Object executeScript = ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", results.get(i));
+                String cardTitle = driver.findElement(
+                        By.xpath("//li[contains(@class,'search-result__occluded-item')]["+1+"]//span[contains(@class, 'actor-name')]")).getText();
+                System.out.println(cardTitle);
+                Assert.assertTrue(cardTitle.contains(searchTerm.toLowerCase()),
+                        "Searchterm " +searchTerm+ "not found in card number"+ Integer.toString(i));
+                //System.out.println(cardTitle);
+            }
+            */
+            // скролим 2 v
+
+
+                for (WebElement result : results) {
+                    ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", result);
+                    String cardTitle = result.getText();
+                    System.out.println("XXXX");
+                    System.out.println(cardTitle);
+                    Assert.assertTrue(cardTitle.toLowerCase().contains(searchTerm),
+                            "Searchterm " +searchTerm+ "not found in cart");
+
+                }
+
+            }
+
+
+            /* пример: WebElement webElement= driver.findElement(By.xpath("Bla-bla-bla"));
+            ( (JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoViev();", webElement); */
+
         }
-    }
 
 
-    }
+
